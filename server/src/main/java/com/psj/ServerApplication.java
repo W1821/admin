8c06f612cce6471a04aa7f4d1623e22f6b6a534b
W1821 <1005136689@qq.com>
2018-05-25 17:45:52 +0800

    基础版本1.0 升级阿里连接池

--- server/src/main/java/com/psj/ServerApplication.java
+++ server/src/main/java/com/psj/ServerApplication.java
@@ -1,17 +1,25 @@
 package com.psj;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import javax.annotation.PostConstruct;
 
 /**
  * @author Administrator
  */
 @SpringBootApplication
+@Slf4j
 public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
     }
+
+    @PostConstruct
+    public void init() {
+        log.info("应用启动");
+    }
+
 }
